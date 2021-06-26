8250273a1f65c7b1442da9da88f21bb829a5e3e1
satyam <satyaom1998@gmail.com>
2021-06-26 22:13:20 +0530

    reduce expiry to 20 for cookies

--- tools/gen_token.js
+++ tools/gen_token.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 //import jwt from 'jsonwebtoken'; 
 
 const generateToken = (res, id, firstname) => {
-  const expiration = 1000*60*60;
+  const expiration = 1000*60*20;
   const token = jwt.sign({ id, firstname }, process.env.JWT_SECRET, {
     expiresIn: '1h',
   });
