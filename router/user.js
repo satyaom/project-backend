241d203e561605db2fbb2b3de812e28997496fbb
satyam <satyaom1998@gmail.com>
2021-06-26 22:59:37 +0530

    post

--- router/user.js
+++ router/user.js
@@ -172,9 +172,7 @@ user.post('/signup', [
 user.get('/getUploads', verifyToken, async (req, res) => {
     try {
         id = req.usr.id;
-        var post
-        while(post == undefined)
-            post = await Post.find({tokenId:req.usr.id});
+        const post = await Post.find({tokenId:req.usr.id});
         q1 = `select public_key from tkey where token_id = "${id}"`;``
         q2 = `select dig_sig from document where token_id = "${id}"`;
         let tamper_list = []
