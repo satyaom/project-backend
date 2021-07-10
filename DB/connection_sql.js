0e6b5f764ef7adee221e32a4c9777d7d74c39f30
satyam <satyaom1998@gmail.com>
2021-07-10 15:57:42 +0530

    fdf

--- DB/connection_sql.js
+++ DB/connection_sql.js
@@ -2,10 +2,10 @@ const mysql = require('mysql')
 
 //create connection
 var conSql = mysql.createPool({
-    host: 'us-cdbr-iron-east-04.cleardb.net',
+    host: 'remotemysql.com',
     user: process.env.USERNAME_SQL, 
     password: process.env.PASSWORD_SQL,
-    database:'heroku_0b096781cffa970' 
+    database:'IEySPYTWPk' 
 });
 
 module.exports = conSql;
